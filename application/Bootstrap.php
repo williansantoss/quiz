61fe48466ca1f81483d55f18a950461442b55c2a
Willian Santos <willian.santos@basis.com.br>
2016-06-23 18:02:14 -0300

    Adicionando localização google maps, alinhamento layoult e adaptação de imagens.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -37,4 +37,38 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		return $view;
 	}
 
+	protected function _initDoctype()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		//Doctype
+		$view->Doctype('XHTML1_TRANSITIONAL');
+		//Css
+		$view->headLink()->prependStylesheet('../css/burger.min.css');
+		$view->headLink()->prependStylesheet('../css/custom.css');
+		$view->headLink()->prependStylesheet('../css/bootstrap.css');
+		$view->headScript()->prependFile('../js/custom.js');
+		$view->headScript()->prependFile('../js/bootstrap.min.js');
+		$view->headLink()->prependStylesheet('../css/bootstrap-responsive.css');
+
+		$view->headLink()->prependStylesheet('css/burger.min.css');
+		$view->headLink()->prependStylesheet('css/custom.css');
+		$view->headLink()->prependStylesheet('css/bootstrap.css');
+		$view->headScript()->prependFile('js/custom.js');
+		$view->headScript()->prependFile('js/bootstrap.min.js');
+		$view->headLink()->prependStylesheet('css/bootstrap-responsive.css');
+
+		$view->headScript()->prependFile('js/jquery.gmap-1.1.0-min.js');
+		$view->headScript()->prependFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');
+		
+
+
+		if (strstr($_SERVER["HTTP_USER_AGENT"], "MSIE")) {
+			$view->headScript()->prependFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');
+		} else {
+			$view->headScript()->prependFile('js/jquery-2.1.3.min.js');
+		}
+
+	}
+
 }
